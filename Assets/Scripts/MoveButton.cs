3955148c34391e5bb312f8a86e6578766ccdd7be
zeffy1014 <zeffy1014@gmail.com>
2020-08-02 02:40:49 +0900

    [update]ボタン押され表現対応
    ボタンを押したときに画像が変わるように対応(画像は既にあった)

--- Assets/Scripts/MoveButton.cs
+++ Assets/Scripts/MoveButton.cs
@@ -14,12 +14,27 @@ public class MoveButton : MonoBehaviour
 
     private bool buttonDown = false;
 
+    [SerializeField] Sprite defaultImage;
+    [SerializeField] Sprite pushImage;
+
     void Start()
     {
         // EventTriggerを追加してボタン押され/離され(PointerDown/PointerUp)を拾う
         var eventTrigger = this.gameObject.AddComponent<ObservableEventTrigger>();
-        eventTrigger.OnPointerDownAsObservable().Subscribe(_ => buttonDown = true).AddTo(this);
-        eventTrigger.OnPointerUpAsObservable().Subscribe(_ => buttonDown = false).AddTo(this);
+        eventTrigger.OnPointerDownAsObservable()
+            .Subscribe(_ =>
+            {
+                buttonDown = true;
+                PushButton(true);
+            })
+        .AddTo(this);
+        eventTrigger.OnPointerUpAsObservable()
+            .Subscribe(_ =>
+            {
+                buttonDown = false;
+                PushButton(false);
+            })
+            .AddTo(this);
 
         // Updateタイミングで現在のボタン押され状況を発行する
         this.UpdateAsObservable()
@@ -30,5 +45,19 @@ public class MoveButton : MonoBehaviour
             .AddTo(this);
     }
 
+    void PushButton(bool startPush)
+    {
+        // 表示画像変更
+        if (startPush)
+        {
+            // 押され画像があればそれにする
+            if (pushImage) this.GetComponent<Image>().sprite = pushImage;
+        }
+        else
+        {
+            // 離され(デフォルト)画像があればそれにする
+            if (defaultImage) this.GetComponent<Image>().sprite = defaultImage;
+        }
+    }
 
 }
